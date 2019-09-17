17a741ac8018d4f11249e8b2a2468bfd753d3d95
summerpassion <695998135@qq.com>
2019-09-17 15:19:45 +0800

    init dev

---
+++ src/Footprint.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * User: summerpassion
+ * DateTime: 2019/9/17 14:41
+ */
+
+namespace Mustang\Footprint;
+
+class Footprint
+{
+
+}
---
+++ src/contracts/Driver.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * User: summerpassion
+ * DateTime: 2019/9/17 15:15
+ */
+namespace Mustang\Footprint;
+
+class Driver
+{
+
+}
